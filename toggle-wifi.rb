88f04437c622c0a467db18e58f943df2b53845ab
Nils Stein <31704359+mietzen@users.noreply.github.com>
2022-09-25 21:01:27 +0200

    Update toggle-wifi.rb

--- toggle-wifi.rb
+++ toggle-wifi.rb
@@ -3,20 +3,20 @@ class ToggleWifi < Formula
   desc "Toggle Wifi On/Off if ethernet is dis-/connected."
   homepage ""
   url "https://github.com/mietzen/toggle-wifi/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "602ee1401539354c9e368dcef0015dcf54fc18e829033fa9ef5f008dcaa07bbc"
+  sha256 "8a645828409ffe0a8e48c5642a6aefea459bcc80652089c6e8d18af036731c9a"
   license "MIT"
 
   depends_on "python@3.10"
   
   def install
-    inreplace "homebrew.mxcl.toggle-wifi.plist", "$TOGGLE-WIFI-PATH$", "#{HOMEBREW_PREFIX}/bin/toggle-wifi"
+    inreplace "service.plist", "$TOGGLE-WIFI-PATH$", "#{HOMEBREW_PREFIX}/bin/toggle-wifi"
     bin.install "toggle-wifi" 
     prefix.install "toggle-wifi.py"
     prefix.install Dir["notifier"]    
     prefix.install "README.md"
     prefix.install "LICENSE"
-    prefix.install "homebrew.mxcl.toggle-wifi.plist"
-    prefix.install_symlink "homebrew.mxcl.toggle-wifi.plist" => "#{plist_name}.plist"
+    prefix.install "service.plist"
+    prefix.install_symlink "service.plist" => "#{plist_name}.plist"
   end
 
   test do
